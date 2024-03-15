1584291ae1f448d9b9bf9cabc2752c4abb54d8f6
Martynas Sklizmantas <saint@ghost.lt>
2024-03-15 20:35:18 +0100

    seems not be used at all

--- app/community/forms.py
+++ app/community/forms.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, HiddenField, SelectField, FileField, \
     DateField
-from wtforms.validators import Min, ValidationError, DataRequired, Email, EqualTo, Length, Optional
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Optional
 from flask_babel import _, lazy_gettext as _l
 
 from app import db
