f8ec37e943f6e59faa9ab55458cbbefd3630e5a5
Martynas Sklizmantas <saint@ghost.lt>
2024-03-15 20:31:36 +0100

    validators try 2

--- app/community/forms.py
+++ app/community/forms.py
@@ -1,10 +1,9 @@
 from flask import request, g
 from flask_login import current_user
 from flask_wtf import FlaskForm
-from validators import Min
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, HiddenField, SelectField, FileField, \
     DateField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Optional
+from wtforms.validators import Min, ValidationError, DataRequired, Email, EqualTo, Length, Optional
 from flask_babel import _, lazy_gettext as _l
 
 from app import db
