83f986d17f7b57656ba0565b8d519c8a6f5c8043
rimu <3310831+rimu@users.noreply.github.com>
2024-03-17 21:00:07 +1300

    try this translation compiler instead

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ sudo systemctl stop celery.service
 git pull
 source venv/bin/activate
 flask db upgrade
-flask translate compile
+pybabel compile -d app/translations
 sudo systemctl start celery.service
 sudo systemctl restart pyfedi.service
