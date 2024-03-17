fed38857f81b09075e86f1132195e7e197a4913d
rimu <3310831+rimu@users.noreply.github.com>
2024-03-17 20:33:29 +1300

    add German language translation data #95

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ sudo systemctl stop celery.service
 git pull
 source venv/bin/activate
 flask db upgrade
+flask translate compile
 sudo systemctl start celery.service
 sudo systemctl restart pyfedi.service
