d076fea6544a8d4cb96a41d849a56124f7143ef9
rimu <3310831+rimu@users.noreply.github.com>
2024-03-27 16:34:15 +1300

    cope with user names with upper case letters

--- app/activitypub/routes.py
+++ app/activitypub/routes.py
@@ -172,7 +172,7 @@ def lemmy_federated_instances():
 def user_profile(actor):
     """ Requests to this endpoint can be for a JSON representation of the user, or a HTML rendering of their profile.
     The two types of requests are differentiated by the header """
-    actor = actor.strip()
+    actor = actor.strip().lower()
     if current_user.is_authenticated and current_user.is_admin():
         if '@' in actor:
             user: User = User.query.filter_by(ap_id=actor).first()
