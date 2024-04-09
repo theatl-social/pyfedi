2d6e17509253f810a80c02ced636f55d68ea13ee
rimu <3310831+rimu@users.noreply.github.com>
2024-04-10 08:43:34 +1200

    remove 404 handler
    a lot of 404s are just images in /static/* and it doesn't make sense to waste cpu cycles presenting a nice page. Also rendering a page requires populating g.site which means hitting the DB.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,9 +3,11 @@ from app import db
 from app.errors import bp
 
 
-@bp.app_errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
+# 404 error handler removed because a lot of 404s are just images in /static/* and it doesn't make sense to waste cpu cycles presenting a nice page.
+# Also rendering a page requires populating g.site which means hitting the DB.
+# @bp.app_errorhandler(404)
+# def not_found_error(error):
+#     return render_template('errors/404.html'), 404
 
 
 @bp.app_errorhandler(500)
