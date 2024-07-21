7b7b0abf17ff98cf4d221ac26e7f77d81017a574
rimu <3310831+rimu@users.noreply.github.com>
2024-07-21 17:04:42 +0800

    allow strikethrough html tag #267

--- app/utils.py
+++ app/utils.py
@@ -220,7 +220,8 @@ def allowlist_html(html: str, a_target='_blank') -> str:
     if html is None or html == '':
         return ''
     allowed_tags = ['p', 'strong', 'a', 'ul', 'ol', 'li', 'em', 'blockquote', 'cite', 'br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre',
-                    'code', 'img', 'details', 'summary', 'table', 'tr', 'td', 'th', 'tbody', 'thead', 'hr', 'span', 'small', 'sub', 'sup']
+                    'code', 'img', 'details', 'summary', 'table', 'tr', 'td', 'th', 'tbody', 'thead', 'hr', 'span', 'small', 'sub', 'sup',
+                    's']
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
 
