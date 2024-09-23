a97ff0cdbd4655d9c47b4a78bcb6481351ac478a
rimu <3310831+rimu@users.noreply.github.com>
2024-09-23 12:08:47 +1200

    refresh packages based on requirements.txt when deploy.sh is run

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ sudo systemctl stop celery.service
 git pull
 source venv/bin/activate
 export FLASK_APP=pyfedi.py
+pip install -r requirements.txt
 flask db upgrade
 pybabel compile -d app/translations
 sudo systemctl start celery.service
