63e36acbcb19458121e56ef52faa99f1d656dce8
Hendrik L <hendrik+dev@xd0.de>
2024-11-25 14:06:56 +0100

    show content as trash only if downvoted

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -972,7 +972,7 @@ def admin_content_trash():
 
     page = request.args.get('page', 1, type=int)
 
-    posts = Post.query.filter(Post.posted_at > utcnow() - timedelta(days=3), Post.deleted == False).order_by(Post.score)
+    posts = Post.query.filter(Post.posted_at > utcnow() - timedelta(days=3), Post.deleted == False, Post.down_votes > 0).order_by(Post.score)
     posts = posts.paginate(page=page, per_page=100, error_out=False)
 
     next_url = url_for('admin.admin_content_trash', page=posts.next_num) if posts.has_next else None
