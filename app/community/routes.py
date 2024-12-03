82ce782be020b1e4a6c35dd2867a4cd29df19007
rimu <3310831+rimu@users.noreply.github.com>
2024-12-03 15:15:22 +1300

    canonical url for communities

--- app/community/routes.py
+++ app/community/routes.py
@@ -331,6 +331,7 @@ def show_community(community: Community):
                            etag=f"{community.id}{sort}{post_layout}_{hash(community.last_active)}", related_communities=related_communities,
                            next_url=next_url, prev_url=prev_url, low_bandwidth=low_bandwidth, un_moderated=un_moderated,
                            recently_upvoted=recently_upvoted, recently_downvoted=recently_downvoted,
+                           canonical=community.profile_id(),
                            rss_feed=f"https://{current_app.config['SERVER_NAME']}/community/{community.link()}/feed", rss_feed_name=f"{community.title} on {g.site.name}",
                            content_filters=content_filters, moderating_communities=moderating_communities(current_user.get_id()),
                            joined_communities=joined_communities(current_user.get_id()),
