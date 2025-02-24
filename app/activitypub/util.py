e0aaf4eb06df68f1e73cb99c0c57239931f3102b
freamon <adslater@gmail.com>
2025-02-24 18:16:55 +0000

    Default to public=True for Feeds fetched from remote sites

--- app/activitypub/util.py
+++ app/activitypub/util.py
@@ -1125,7 +1125,8 @@ def actor_json_to_model(activity_json, address, server):
                               ap_fetched_at=utcnow(),
                               ap_domain=server.lower(),
                               public_key=activity_json['publicKey']['publicKeyPem'],
-                              instance_id=find_instance_id(server)
+                              instance_id=find_instance_id(server),
+                              public=True
                               )
 
         description_html = ''
