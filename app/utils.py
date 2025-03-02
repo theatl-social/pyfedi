cdbb21d5a91a34a1f128eca9439746c553c8bffa
rimu <3310831+rimu@users.noreply.github.com>
2025-03-03 04:22:24 +1300

    guard against user is None

--- app/utils.py
+++ app/utils.py
@@ -1382,7 +1382,7 @@ def actor_profile_contains_blocked_words(user: User) -> bool:
     if blocked_words and blocked_words.strip() != '':
         for blocked_word in blocked_words.split('\n'):
             blocked_word = blocked_word.lower().strip()
-            if user.about_html and blocked_word in user.about_html:
+            if user.about_html and blocked_word in user.about_html.lower():
                 return True
     return False
 
