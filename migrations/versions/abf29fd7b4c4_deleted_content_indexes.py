fc8447214c061de6149ad6c331b20129d516d66d
rimu <3310831+rimu@users.noreply.github.com>
2025-04-08 15:16:34 +1200

    indexes to speed up daily maintenance task

---
+++ migrations/versions/abf29fd7b4c4_deleted_content_indexes.py
@@ -0,0 +1,47 @@
+"""deleted content indexes
+
+Revision ID: abf29fd7b4c4
+Revises: ae44a91945d0
+Create Date: 2025-04-08 15:10:07.728100
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'abf29fd7b4c4'
+down_revision = 'ae44a91945d0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+            CREATE INDEX ix_post_reply_community_id_not_deleted
+            ON post_reply (community_id)
+            WHERE deleted = false;
+        """)
+    op.execute("""
+                CREATE INDEX ix_post_community_id_not_deleted
+                ON post (community_id)
+                WHERE deleted = false;
+            """)
+
+    op.execute("""
+                    CREATE INDEX ix_post_user_id_not_deleted
+                    ON post (user_id)
+                    WHERE deleted = false;
+                """)
+
+
+def downgrade():
+    op.execute("""
+            DROP INDEX ix_post_reply_community_id_not_deleted;
+        """)
+    op.execute("""
+                DROP INDEX ix_post_community_id_not_deleted;
+            """)
+    op.execute("""
+                    DROP INDEX ix_post_user_id_not_deleted;
+                """)
