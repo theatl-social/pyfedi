c711fb8fca2debe9ad437ef798d7f5d7f0139c5c
rimu <3310831+rimu@users.noreply.github.com>
2025-04-26 16:22:56 +1200

    notify about posts in feed

--- app/activitypub/util.py
+++ app/activitypub/util.py
@@ -1847,7 +1847,25 @@ def notify_about_post_task(post_id):
             user = User.query.get(notify_id)
             user.unread_notifications += 1
             db.session.commit()
-            notifications_sent_to.add(notify_id)    
+            notifications_sent_to.add(notify_id)
+
+
+    # NOTIF_FEED
+    # Get all the feeds that the post's community is in
+    community_feeds = Feed.query.join(FeedItem, FeedItem.feed_id == Feed.id).filter(FeedItem.community_id == post.community_id).all()
+
+    for feed in community_feeds:
+        feed_send_notifs_to = notification_subscribers(feed.id, NOTIF_FEED)
+        for notify_id in feed_send_notifs_to:
+            if notify_id != post.user_id and notify_id not in notifications_sent_to:
+                new_notification = Notification(title=shorten_string(post.title, 50), url=f"/post/{post.id}",
+                                                user_id=notify_id, author_id=post.user_id,
+                                                notif_type=NOTIF_FEED)
+                db.session.add(new_notification)
+                user = User.query.get(notify_id)
+                user.unread_notifications += 1
+                db.session.commit()
+                notifications_sent_to.add(notify_id)
 
 
 def notify_about_post_reply(parent_reply: Union[PostReply, None], new_reply: PostReply):
