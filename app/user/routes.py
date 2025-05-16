17811316667404b4b9ccc118b66446b333102d03
rimu <3310831+rimu@users.noreply.github.com>
2025-05-16 18:33:52 +1200

    unread notification red dot

--- app/user/routes.py
+++ app/user/routes.py
@@ -888,6 +888,7 @@ def notifications():
     notification_types = defaultdict(int)
     notification_links = defaultdict(set)
     notification_list = Notification.query.filter_by(user_id=current_user.id).order_by(desc(Notification.created_at)).limit(100).all()
+    # Build a list of the types of notifications this person has, by going through all their notifications
     for notification in notification_list:
         has_notifications = True
         if notification.notif_type != NOTIF_DEFAULT:
