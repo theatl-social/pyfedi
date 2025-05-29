3a566f531425758bcd99527a36c897e61174205b
rimu <3310831+rimu@users.noreply.github.com>
2025-05-29 16:07:45 +1200

    service worker caching - bootstrap and more fonts

--- app/static/service_worker.js
+++ app/static/service_worker.js
@@ -1,31 +1,42 @@
 //https://developers.google.com/web/fundamentals/primers/service-workers
 
-// Font files
-var fontFiles = [
-	'/static/fonts/feather/feather.ttf',
+var staticFiles = [
+  '/static/fonts/feather/feather.ttf',
+  '/static/fonts/feather/feather.woff',
+  '/bootstrap/static/css/bootstrap.min.css',
 ];
 
-//this is just an empty service worker so that the 'Install CB as an app' prompt appears in web browsers
 self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open('core').then(function (cache) {
-		fontFiles.forEach(function (file) {
-			cache.add(new Request(file));
-		});
-		return;
-	}));
+  event.waitUntil(
+    caches.open('core').then(function (cache) {
+      return cache.addAll(staticFiles);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  const request = event.request;
+  if (request.url.includes('/static/fonts/feather/')) {
+    event.respondWith(
+      caches.match(request).then(function(response) {
+        return response || fetch(request).then(function(fetchResponse) {
+          return fetchResponse;
+        });
+      })
+    );
+  }
 });
 
 self.addEventListener('fetch', function(event) {
-    // Fonts
-    // Offline-first
-    if (request.url.includes('feather.ttf')) {
-        event.respondWith(
-            caches.match(request).then(function (response) {
-                return response || fetch(request).then(function (response) {
-                    // Return the requested file
-                    return response;
-                });
-            })
-        );
-    }
+  const request = event.request;
+  const url = request.url;
+
+  if (url.includes('/static/fonts/feather/') || url.includes('/bootstrap/static/css/bootstrap.min.css')) {
+    event.respondWith(
+      caches.match(request).then(function (response) {
+        return response || fetch(request);
+      })
+    );
+  }
 });
