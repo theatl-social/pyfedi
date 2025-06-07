e8deb064f628f2872bcaa39bf2d0282e45680961
rimu <3310831+rimu@users.noreply.github.com>
2025-06-08 10:14:17 +1200

    force server_name to lowercase

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SERVER_NAME = os.environ.get('SERVER_NAME') or 'localhost'
+    SERVER_NAME = os.environ.get('SERVER_NAME').lower() or 'localhost'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guesss'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
