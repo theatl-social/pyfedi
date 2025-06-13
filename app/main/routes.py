70dcb1136c9c2b7873064ee98eb1cd7b5dc1cb2b
Walter Schwenger <wjs018@gmail.com>
2025-06-13 12:53:30 -0400

    Fixed broken routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -294,6 +294,17 @@ def list_local_communities():
     if len(public_feeds) > 0:
         server_has_feeds = True
 
+    try:
+        site = g.site
+    except:
+        site = Site.query.get(1)
+    create_admin_only = site.community_creation_admin_only
+
+    if current_user.is_authenticated:
+        is_admin = current_user.is_admin()
+    else:
+        is_admin = False
+
     # if filtering by public feed
     # get all the ids of the communities
     # then filter the communities to ones whose ids match the feed
@@ -326,7 +337,7 @@ def list_local_communities():
     return render_template('list_communities.html', communities=communities, search=search_param, title=_('Local Communities'),
                            SUBSCRIPTION_PENDING=SUBSCRIPTION_PENDING, SUBSCRIPTION_MEMBER=SUBSCRIPTION_MEMBER,
                            SUBSCRIPTION_OWNER=SUBSCRIPTION_OWNER, SUBSCRIPTION_MODERATOR=SUBSCRIPTION_MODERATOR,
-                           next_url=next_url, prev_url=prev_url, current_user=current_user,
+                           next_url=next_url, prev_url=prev_url, current_user=current_user, create_admin_only=create_admin_only, is_admin=is_admin,
                            topics=topics, languages=languages, topic_id=topic_id, language_id=language_id, sort_by=sort_by,
                            joined_communities=joined_or_modding_communities(current_user.get_id()),
                            pending_communities=pending_communities(current_user.get_id()),
@@ -380,6 +391,17 @@ def list_subscribed_communities():
     public_feeds = Feed.query.filter_by(public=True).order_by(Feed.title).all()
     if len(public_feeds) > 0:
         server_has_feeds = True
+    
+    try:
+        site = g.site
+    except:
+        site = Site.query.get(1)
+    create_admin_only = site.community_creation_admin_only
+
+    if current_user.is_authenticated:
+        is_admin = current_user.is_admin()
+    else:
+        is_admin = False
 
     # if filtering by public feed
     # get all the ids of the communities
@@ -406,7 +428,7 @@ def list_subscribed_communities():
     return render_template('list_communities.html', communities=communities, search=search_param, title=_('Joined Communities'),
                            SUBSCRIPTION_PENDING=SUBSCRIPTION_PENDING, SUBSCRIPTION_MEMBER=SUBSCRIPTION_MEMBER,
                            SUBSCRIPTION_OWNER=SUBSCRIPTION_OWNER, SUBSCRIPTION_MODERATOR=SUBSCRIPTION_MODERATOR,
-                           next_url=next_url, prev_url=prev_url, current_user=current_user,
+                           next_url=next_url, prev_url=prev_url, current_user=current_user, create_admin_only=create_admin_only, is_admin=is_admin,
                            topics=topics, languages=languages, topic_id=topic_id, language_id=language_id, sort_by=sort_by,
                            joined_communities=joined_or_modding_communities(current_user.get_id()),
                            pending_communities=pending_communities(current_user.get_id()),
@@ -457,6 +479,17 @@ def list_not_subscribed_communities():
     public_feeds = Feed.query.filter_by(public=True).order_by(Feed.title).all()
     if len(public_feeds) > 0:
         server_has_feeds = True
+    
+    try:
+        site = g.site
+    except:
+        site = Site.query.get(1)
+    create_admin_only = site.community_creation_admin_only
+
+    if current_user.is_authenticated:
+        is_admin = current_user.is_admin()
+    else:
+        is_admin = False
 
     # if filtering by public feed
     # get all the ids of the communities
@@ -487,7 +520,7 @@ def list_not_subscribed_communities():
     return render_template('list_communities.html', communities=communities, search=search_param, title=_('Not Joined Communities'),
                            SUBSCRIPTION_PENDING=SUBSCRIPTION_PENDING, SUBSCRIPTION_MEMBER=SUBSCRIPTION_MEMBER,
                            SUBSCRIPTION_OWNER=SUBSCRIPTION_OWNER, SUBSCRIPTION_MODERATOR=SUBSCRIPTION_MODERATOR,
-                           next_url=next_url, prev_url=prev_url, current_user=current_user,
+                           next_url=next_url, prev_url=prev_url, current_user=current_user, create_admin_only=create_admin_only, is_admin=is_admin,
                            topics=topics, languages=languages, topic_id=topic_id, language_id=language_id, sort_by=sort_by,
                            joined_communities=joined_or_modding_communities(current_user.get_id()),
                            pending_communities=pending_communities(current_user.get_id()),
