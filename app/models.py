3c6bbaad4101242978b5fbfe0a2a8e7da695bfbb
freamon <adslater@gmail.com>
2025-07-05 09:42:19 +0000

    Add password_updated_at to User model, #963

--- app/models.py
+++ app/models.py
@@ -873,6 +873,7 @@ class User(UserMixin, db.Model):
     additional_css = db.Column(db.Text)
     mastodon_oauth_id = db.Column(db.String(64), unique=True, index=True)
     discord_oauth_id = db.Column(db.String(64), unique=True, index=True)
+    password_updated_at = db.Column(db.DateTime, default=utcnow)
 
     avatar = db.relationship('File', lazy='joined', foreign_keys=[avatar_id], single_parent=True, cascade="all, delete-orphan")
     cover = db.relationship('File', lazy='joined', foreign_keys=[cover_id], single_parent=True, cascade="all, delete-orphan")
