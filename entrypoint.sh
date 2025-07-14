01f093fb6bc0722936237666ade99c0a1d615794
Michael Handelman <m@mikeh.dev>
2025-07-14 13:32:10 -0400

    editing entrypoints

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ flask populate_community_search
 if [ "${FLASK_DEBUG:-}" = "1" ] && [ "${FLASK_ENV:-}" = "development" ]; then
   export FLASK_RUN_EXTRA_FILES=$(find app/templates app/static -type f | tr '\n' ':')
   echo "Starting flask development server..."
-  flask run -h 0.0.0.0 -p 5000
+  python3 -m flask run -h 0.0.0.0 -p 5000
 else
   echo "Starting Gunicorn..."
-  gunicorn --config gunicorn.conf.py --preload pyfedi:app
+  python3 -m gunicorn --config gunicorn.conf.py --preload pyfedi:app
 fi
