a3e4e345b1c7893b7ac86b1e4ee9437bad1aa991
Michael Handelman <m@mikeh.dev>
2025-07-16 08:48:12 -0400

    edited script files to remove venv

--- email_notifs.sh
+++ email_notifs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source venv/bin/activate
+
 export FLASK_APP=pyfedi.py
 python3 -m flask send_missed_notifs
 python3 -m flask process_email_bounces
--- send_queue.sh
+++ send_queue.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-source venv/bin/activate
 export FLASK_APP=pyfedi.py
 python3 -m flask send-queue
