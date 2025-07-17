b2a21da8eb109fedd24bef7ea8862df33cb96346
Michael Handelman <m@mikeh.dev>
2025-07-16 21:49:18 -0400

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,3 @@
-# #!/usr/bin/env sh
-# set -e
-
-# export FLASK_APP=pyfedi.py
-
-# echo "Starting cron daemon..."
-# cron
-
-# echo "Running database migrations..."
-# python3 -m flask db upgrade
-
-# python3 -m flask populate_community_search
-
-# if [ "${FLASK_DEBUG:-}" = "1" ] && [ "${FLASK_ENV:-}" = "development" ]; then
-#   export FLASK_RUN_EXTRA_FILES=$(find app/templates app/static -type f | tr '\n' ':')
-#   echo "Starting flask development server..."
-#   python3 -m flask run -h 0.0.0.0 -p 5000
-# else
-#   echo "Starting Gunicorn..."
-#   python3 -m gunicorn --config gunicorn.conf.py --preload pyfedi:app
-# fi
-
 #!/usr/bin/env sh
 set -e
 
