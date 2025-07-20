39fce0250f6b8e090497e7df45a5266037027588
rimu <3310831+rimu@users.noreply.github.com>
2025-07-21 10:17:48 +1200

    increase textarea height as people type

--- app/user/forms.py
+++ app/user/forms.py
@@ -13,7 +13,7 @@ class ProfileForm(FlaskForm):
     email = EmailField(_l('Email address'), validators=[Email(), DataRequired(), Length(min=5, max=255)])
     password = PasswordField(_l('Set new password'), validators=[Optional(), Length(min=8, max=129)],
                              render_kw={'autocomplete': 'new-password', 'title': _l('Minimum length 8, maximum 128')})
-    about = TextAreaField(_l('Bio'), validators=[Optional(), Length(min=3, max=5000)], render_kw={'rows': 5})
+    about = TextAreaField(_l('Bio'), validators=[Optional(), Length(min=3, max=5000)], render_kw={'rows': 5, 'class': 'autoresize'})
     extra_label_1 = StringField(_l('Extra field 1 - label'), validators=[Optional(), Length(max=50)], render_kw={"placeholder": _l('Label')})
     extra_text_1 = StringField(_l('Extra field 1 - text'), validators=[Optional(), Length(max=256)], render_kw={"placeholder": _l('Content')})
     extra_label_2 = StringField(_l('Extra field 2 - label'), validators=[Optional(), Length(max=50)], render_kw={"placeholder": _l('Label')})
