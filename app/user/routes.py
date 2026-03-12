894beccf9140f87f706c6d98d7fe7d3fc9397e7a
MortySmith <benedikt@alhujaj.de>
2026-03-12 18:24:46 +0100

    Implemented a way to filter unread notifications

--- app/user/routes.py
+++ app/user/routes.py
@@ -1116,6 +1116,7 @@ def notifications():
     type_ = request.args.get('type', '')
     current_filter = type_
     has_notifications = False
+    filter_unread = request.args.get('filter_unread', 'False')
 
     notification_types = defaultdict(int)
     notification_links = defaultdict(set)
@@ -1139,7 +1140,7 @@ def notifications():
     return render_template('user/notifications.html', title=_('Notifications'), notifications=notification_list,
                            notification_types=notification_types, has_notifications=has_notifications,
                            user=current_user, notification_links=notification_links, current_filter=current_filter,
-                           site=g.site, markdown_to_html=markdown_to_html,
+                           filter_unread=filter_unread, site=g.site, markdown_to_html=markdown_to_html,
                            )
 
 
