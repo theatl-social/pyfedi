6ff4ff86653940dd41ed0fe25ea24bbf3414e08d
rimu <rimu@rimu.geek.nz>
2026-04-03 14:40:57 +1300

    api: post list perf using a materialized view - minor bugs #1724

--- app/api/alpha/utils/post.py
+++ app/api/alpha/utils/post.py
@@ -333,7 +333,7 @@ def get_post_list(auth, data, user_id=None, search_type='Posts') -> dict:
             posts = posts.filter(Post.id.in_(upvoted_post_ids), Post.user_id != user_id)
             
             # SQL query building for liked_only
-            post_query_criteria.append('id IN :upvoted_post_ids')
+            post_query_criteria.append('post_id IN :upvoted_post_ids')
             post_query_criteria.append('user_id != :user_id')
             post_query_parameters['upvoted_post_ids'] = tuple(upvoted_post_ids)
             post_query_parameters['user_id'] = user_id
@@ -384,7 +384,7 @@ def get_post_list(auth, data, user_id=None, search_type='Posts') -> dict:
                     )
                 )
                 # SQL query building for hide_read_posts
-                post_query_criteria.append('id NOT IN :read_post_ids')
+                post_query_criteria.append('post_id NOT IN :read_post_ids')
                 post_query_parameters['read_post_ids'] = tuple(u_rp_ids)
 
         filtered_out_community_ids = filtered_out_communities(user)
